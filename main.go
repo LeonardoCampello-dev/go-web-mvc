d79757f0f853cdadc97a4f96be833159f67f4948
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-06-16 11:05:48 -0300

    connecting with postgreSQL

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"net/http"
 	"text/template"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
 
 type Product struct {
@@ -15,9 +19,25 @@ type Product struct {
 var htmlTemplates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := connectDB()
+
+	defer db.Close()
+
 	http.HandleFunc("/", index)
 
 	http.ListenAndServe(":8000", nil)
+}
+
+func connectDB() *sql.DB {
+	connection := "user=leonardo-dev dbname=go_store password=postgres host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
